2b75eb209ca68f9818b8243684a87557ce48147f
Yuvraj Tayal <126242716+yuvrajtayal1202@users.noreply.github.com>
2025-03-23 04:46:53 +0000

    done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,2 +1,22 @@
+
 const myArr = [1, 2, 3, 4, 5, true, "Yuvraj"]
 console.log(myArr);
+
+
+// Array methods
+myArr.push("Rajiv") // adds values in last
+myArr.pop() // removes value from last 
+
+myArr.unshift("go") // adds value in first position
+myArr.shift() // removes value from first postion
+
+
+const newArr = myArr.join() // converts array to string
+const CnewArr = myArr.join('') // to remove commas
+console.log(CnewArr);
+
+
+console.log("A ", myArr)
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+
